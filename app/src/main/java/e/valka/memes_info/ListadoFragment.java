b844c4644218c2158babd584a0c873b581555cf9
aterron71 <aterron71@github.com>
2018-09-21 13:22:31 -0500

    Primer commit

---
+++ app/src/main/java/e/valka/memes_info/ListadoFragment.java
@@ -0,0 +1,88 @@
+package e.valka.memes_info;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ListadoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate (R.layout.fragment_listado, container, false);
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        FragmentActivity activity = getActivity ();
+        if (activity == null) return;
+
+        RecyclerView recyclerView = activity.findViewById (R.id.rvListado);
+        if (recyclerView == null) return;
+
+        String[] texto = getResources().getStringArray(R.array.names);
+        TypedArray imagenes = getResources().obtainTypedArray(R.array.images);
+
+
+        recyclerView.setLayoutManager (new LinearLayoutManager(getContext (), LinearLayout.VERTICAL, false));
+        recyclerView.setAdapter (new  ListadoAdapter (getContext (), texto,imagenes));
+    }
+}
+
+class ListadoAdapter extends RecyclerView.Adapter<ListadoViewHolder> {
+    private Context context;
+    private String[] myData;
+    private TypedArray imagenes;
+
+    ListadoAdapter (Context context, String[] myData,TypedArray imagenes) {
+        this.context = context;
+        this.myData = myData;
+        this.imagenes = imagenes;
+    }
+
+    @NonNull
+    @Override
+    public ListadoViewHolder onCreateViewHolder (@NonNull ViewGroup viewGroup, int i) {
+        View rowView = LayoutInflater.from (context).inflate (R.layout.fragment_item, viewGroup, false);
+        return new ListadoViewHolder (rowView);
+    }
+
+    @Override
+    public void onBindViewHolder (@NonNull ListadoViewHolder listadoViewHolder, int i) {
+        listadoViewHolder.bind (myData[i],imagenes.getResourceId(i,-1));
+    }
+
+    @Override
+    public int getItemCount () {
+        return myData.length;
+    }
+
+}
+
+class ListadoViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+    private ImageView imageView;
+
+    ListadoViewHolder (@NonNull View itemView) {
+        super (itemView);
+        imageView = itemView.findViewById(R.id.imagen);
+        textView = itemView.findViewById (R.id.textoImagen);
+    }
+
+    public void bind (String text, int imagen) {
+        textView.setText (text);
+        imageView.setImageResource(imagen);
+    }
+}
